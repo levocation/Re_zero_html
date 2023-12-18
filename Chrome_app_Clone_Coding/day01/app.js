b6b062380c0c5fd2d51a07617fe8e1290875e15f
QUOTIENT <tjsgh5965@naver.com>
2023-12-18 17:04:30 +0900

    day01-office

---
+++ Chrome_app_Clone_Coding/day01/app.js
@@ -0,0 +1,34 @@
+alert("hi");
+
+console.log(545445);
+console.log('hihi');
+console.log("hehe");
+
+const a = 5;
+// const는 constant의 줄임말로
+// 변하지 않는 상수를 뜻함.
+
+let l = 3;
+// 값을 업데이트할 수 있는 변수 let
+console.log(a + 2);
+console.log(a - 2);
+console.log(a * 2);
+console.log(a / 2);
+
+const t = true;
+// Boolean
+
+const nl = null;
+// null value
+
+let someString;
+// undefined value
+
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// array
+
+console.log(dayOfWeek[0]);
+// "mon"
+
+dayOfWeek.push("sun");
+// add value
